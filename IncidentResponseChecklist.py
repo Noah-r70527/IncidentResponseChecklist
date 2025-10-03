d7775d52142a236af06e0bfdde180bb383ef207e
noah-rob7852 <72880561+noah-rob7852@users.noreply.github.com>
2025-10-03 14:35:23 -0400

    Init Commit

---
+++ IncidentResponseChecklist.py
@@ -0,0 +1,18 @@
+import customtkinter as ctk
+from gui.IncidentResponseChecklistGUI import *
+
+if __name__ == "__main__":
+    try:
+        root = ctk.CTk()
+        root.title("IP Lookup Tool")
+        app = IPApp(root, riskyip=handler)
+        root.mainloop()
+
+    except ModuleNotFoundError as e:
+        input(f'Missing a module: {e}. Trying running pip -r requirements.txt and then run the script again.')
+
+    except FileNotFoundError as e:
+        input(f'Error occurred during initialization: {e}')
+
+    except Exception as e:
+        input(f'Unhandled exception occurred.: {e}')
