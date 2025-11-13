8e412f61fb6cbd26b50e76a52a20f07f967bc9fb
noah-rob7852 <72880561+noah-rob7852@users.noreply.github.com>
2025-11-12 21:06:25 -0500

    Fixed an error with the csv saving logic

--- utils/Utils.py
+++ utils/Utils.py
@@ -4,11 +4,15 @@ def output_to_csv(csv_file_name: str, csv_file_content: list[dict[str, str]]) ->
 
     try:
         with open(f'output/{csv_file_name}', "w", encoding="utf-8", newline='') as csv_file:
-            headers = csv_file_content[0].keys()
-            writer = csv.DictWriter(csv_file, fieldnames=headers, extrasaction="ignore")
-            writer.writeheader()
-            writer.writerows(csv_file_content)
-            return True
+            writer = csv.writer(csv_file)
+            writer.writerow(["Step Description", "Step Results"])
+
+            for row in csv_file_content:
+                key, value = list(row.items())[0]
+                writer.writerow([key, value])
+
+        return True
 
     except Exception as e:
+        print("Error:", e)
         return False
